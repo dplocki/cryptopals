6d943e7c5c4ca05b02fc02446c7e617ebfc73352
Dawid Płocki <dawid.plocki@gmail.com>
2022-01-29 14:41:35 +0100

    Solution for set1/challenge02

--- core.go
+++ core.go
@@ -11,3 +11,16 @@ func HexDecodeToBase64(input string) string {
 
 	return result
 }
+
+func EncodeFixedXor(input string, key string) string {
+	input_byte, _ := hex.DecodeString(input)
+	key_byte, _ := hex.DecodeString(key)
+	output_byte := make([]byte, len(input_byte))
+
+	for i, value := range input_byte {
+		output_byte[i] = value ^ key_byte[i]
+	}
+
+	result := hex.EncodeToString(output_byte)
+	return result
+}
--- core_test.go
+++ core_test.go
@@ -5,7 +5,15 @@ import "testing"
 func TestHexDecodeToBase64(t *testing.T) {
 	result := HexDecodeToBase64("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")
 
-	if result == "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t" {
+	if result != "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t" {
+		t.Error("innocorrect result", result)
+	}
+}
+
+func TestEncodeFixedXor(t *testing.T) {
+	result := EncodeFixedXor("1c0111001f010100061a024b53535009181c", "686974207468652062756c6c277320657965")
+
+	if result != "746865206b696420646f6e277420706c6179" {
 		t.Error("innocorrect result", result)
 	}
 }
