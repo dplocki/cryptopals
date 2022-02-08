6348efd935f2a3ed4d8b5b3ad6f4a94715ca31c7
Dawid Płocki <dawid.plocki@gmail.com>
2022-02-08 19:52:12 +0100

    Solution for set1/challenge08

--- set1_challenge08.go
+++ set1_challenge08.go
@@ -1,15 +1,60 @@
 package main
 
-import (
-	"encoding/hex"
-	"fmt"
-)
+import "encoding/hex"
+
+type ProductResult struct {
+	FirstField  int
+	SecondField int
+}
+
+func product(n int) <-chan ProductResult {
+	chanel := make(chan ProductResult)
+	go func() {
+		for firstIndex := 0; firstIndex < n-1; firstIndex++ {
+			for secondIndex := firstIndex + 1; secondIndex < n; secondIndex++ {
+				result := new(ProductResult)
+				result.FirstField = firstIndex
+				result.SecondField = secondIndex
+
+				chanel <- *result
+			}
+		}
+
+		close(chanel)
+	}()
+
+	return chanel
+}
+
+func compeareBlock(message []byte, firstIndex, secondIndex, blockSize int) bool {
+	for i := 0; i < blockSize; i++ {
+		if message[firstIndex*blockSize+i] != message[secondIndex*blockSize+i] {
+			return false
+		}
+	}
+
+	return true
+}
+
+func isDecryptedByECD(message []byte, blockSize int) bool {
+	for productResult := range product(len(message) / blockSize) {
+		if compeareBlock(message, productResult.FirstField, productResult.SecondField, blockSize) {
+			return true
+		}
+	}
+
+	return false
+}
 
 func MainSet1Challenge08() {
 	content := LoadFileContentAsStringsArray("8.txt")
 
-	for _, v := range content {
-		inputByte, _ := hex.DecodeString(v)
-		fmt.Println(inputByte[0:16])
+	for lineNumber, value := range content {
+		inputByte, _ := hex.DecodeString(value)
+
+		if isDecryptedByECD(inputByte, 16) {
+			println("Line number", lineNumber)
+			println(value)
+		}
 	}
 }
