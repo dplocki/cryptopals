7f6503feb873884211cfd0645146d36c0a449688
Dawid Płocki <dawid.plocki@gmail.com>
2022-03-29 17:44:38 +0200

    Draft for solution of set2/challenge13: implement encoding routine

--- set2_challenge13.go
+++ set2_challenge13.go
@@ -2,27 +2,47 @@ package main
 
 import "strings"
 
-func splitQueryParamaters(input string) map[string]string {
+func fromQueryParameters(input string) map[string]string {
 	result := make(map[string]string)
 	input_split := strings.Split(input, "&")
 
-	for _, v := range input_split {
-		v_split := strings.Split(v, "=")
-		result[v_split[0]] = v_split[1]
+	for _, value := range input_split {
+		valueSplit := strings.Split(value, "=")
+		result[valueSplit[0]] = valueSplit[1]
 	}
 
 	return result
+}
+
+func toQueryParameters(input map[string]string) string {
+	result := strings.Builder{}
+	index := 0
+
+	for key, value := range input {
+		result.WriteString(key)
+		result.WriteRune('=')
+		result.WriteString(value)
+		index++
+
+		if index != len(input) {
+			result.WriteRune('&')
+		}
+	}
+
+	return result.String()
 }
 
 func MainSet2Challenge13() {
 	println("ECB cut-and-paste")
 
 	input := "foo=bar&baz=qux&zap=zazzle"
-	query_param_map := splitQueryParamaters(input)
+	query_param_map := fromQueryParameters(input)
 
 	println("{")
 	for k, v := range query_param_map {
 		println("\t", k, ":", v)
 	}
 	println("}")
+
+	println(toQueryParameters(query_param_map))
 }
